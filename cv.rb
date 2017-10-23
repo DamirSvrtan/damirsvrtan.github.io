1e1db98107b704e03a68e0932e45c00e296ecfab
Damir Svrtan <damir.svrtan@gmail.com>
2017-10-23 21:11:20 +0200

    Update talks and blogposts

--- cv.rb
+++ cv.rb
@@ -11,38 +11,39 @@ Jobless.cv do
     entry do
       title "Rails Team Lead"
       company "Infinum"
-      technologies "Ruby, Ruby on Rails"
-      bulletin "Leading a team of 8 Rails developers"
-      bulletin "Instituting best practices such as code reviews, pull requests and automated testing"
-      bulletin "Enforcing overall code quality"
-      bulletin "Leading a startup featured on TechCrunch Disrupt London 2015"
+      technologies "Ruby, Ruby on Rails, Elixir, Phoenix"
       start_date "April 2015"
       end_date "Present"
+
+      bulletin "Leading a team of Rails developers (grown from 4 to 8 members)"
+      bulletin "Instituting best practices such as code reviews, pull requests and automated testing"
+      bulletin "Increasing deliverability and stability of projects by enforcing overall code quality"
+      bulletin "Increasing overall team knowledge by mentoring and onboarding new Rails developers<br/><br/>"
+
+      bulletin "Software architecture decision making and technical leadership on web based projects"
+      bulletin "Leading a startup featured on TechCrunch Disrupt London 2015"
+      bulletin "Highly involved in the recruitment and hiring process"
     end
+
+    entry do
+      title "Rails course instructor"
+      company "Infinum Student Academy"
+      start_date "July 2015"
+      end_date "August 2015"
+      bulletin "Held lectures on a month long course on web app development with Ruby On Rails"
+      bulletin "Coordinated and led a team responsible for creating and reviewing students homework"
+    end
+
     entry do
       title "Rails Developer"
       company "Infinum"
-      technologies "Ruby, Ruby on Rails, Javascript"
-      bulletin "Building various software for international clients"
-      bulletin "Building a Ruby SDK for the Stormpath Authentication Service"
-      bulletin "Building Phrasing, an open source inline content editing Rails engine"
+      technologies "Ruby, Ruby on Rails, Javascript, Node.js"
       start_date "July 2013"
       end_date "April 2015"
-    end
-  end
 
-  education do
-    entry do
-      title "M.S. Software Engineering"
-      company "The University of Zagreb"
-      start_date "2012"
-      end_date "2014"
-    end
-    entry do
-      title "B.S. Software Engineering"
-      company "The University of Zagreb"
-      start_date "2008"
-      end_date "2012"
+      bulletin "Building web applications for U.S. based startups and well established "
+      bulletin "Building the Ruby SDK for the Stormpath Authentication Service (a Silicon Valley startup)"
+      bulletin "Developed Phrasing, an open source inline content editing Rails engine"
     end
   end
 
@@ -50,32 +51,28 @@ Jobless.cv do
     entry do
       title "Ruby Zagreb Meetup organizer"
       homepage "http://www.meetup.com/rubyzg/"
+      start_date "March 2014"
+      end_date "Present"
     end
     entry do
-      title "Volunteer help @ Webcamp Zagreb (the largest Web conference in the Balkans), 2014-2016"
-      homepage "https://2016.webcampzg.org/"
-    end
-    entry do
-      title "Stack Overflow contributor (Top 9%)"
+      title "Stack Overflow contributor (Top 8%)"
       homepage "http://stackoverflow.com/users/1279707/zippie?tab=profile"
     end
   end
 
-  open_source do
-    github_repo "DamirSvrtan/fasterer"
-    github_repo "infinum/phrasing"
-  end
-
-  group "Certificates" do
-    entry do
-      title "EDX, BerkleyX: CS169.2x: Advanced Software as a Service"
-    end
-    entry do
-      title "EDX, BerkleyX: CS169.1x: Engineering Software as a Service"
-    end
-  end
-
   group "Talks" do
+    entry do
+      title 'Building Ruby Bots on AWS Lambda @ NYC.rb 2017'
+      homepage 'https://www.meetup.com/NYC-rb/events/235117574/'
+    end
+    entry do
+      title 'Stateless Authentication w/ JSON Web Tokens @ South East Ruby Nashville 2017'
+      homepage 'https://southeastruby.com/'
+    end
+    entry do
+      title 'Stop Wasting Time Not Writing Tests @ We Are Developers Vienna 2017'
+      homepage 'https://www.wearedevelopers.com'
+    end
     entry do
       title "Reinventing the Bootcamp Idea @ Rails Israel 2016"
       homepage "https://railsisrael2016.events.co.il/tracks"
@@ -117,9 +114,18 @@ Jobless.cv do
       description "Article featured in the Ruby Weekly, Ruby5 podcast and Green Ruby newsletter"
     end
     entry do
+      title "Don't send proposals at the last moment"
+      homepage 'https://2017.webcampzg.org/news/dont-send-proposals-last-moment/'
+      description "Article featured in the Technically Speaking newsletter"
+    end
+    entry do
       title "Top 8 tools for Ruby on Rails code optimization and cleanup"
       homepage "https://infinum.co/the-capsized-eight/articles/top-8-tools-for-ruby-on-rails-code-optimization-and-cleanup"
       description "Article featured in the Ruby Weekly, Ruby5 podcast and Green Ruby newsletter"
+    end
+    entry do
+      title "Better login forms without security risks"
+      homepage 'https://infinum.co/the-capsized-eight/better-login-forms-without-security-risks'
     end
     entry do
       title "Tips for participating in the Rails Rumble"
@@ -135,6 +141,36 @@ Jobless.cv do
       title "Whitelisting with the lesser known presence in method"
       homepage "https://infinum.co/the-capsized-eight/articles/whitelisting-with-the-lesser-known-presence-in-method"
       description "Article featured in the Ruby Weekly, Ruby5 podcast and Green Ruby newsletter"
+    end
+  end
+
+  open_source do
+    github_repo "DamirSvrtan/fasterer"
+    github_repo "infinum/rails-handbook"
+    github_repo "infinum/phrasing"
+  end
+
+  education do
+    entry do
+      title "M.S. Software Engineering"
+      company "The University of Zagreb"
+      start_date "2012"
+      end_date "2014"
+    end
+    entry do
+      title "B.S. Software Engineering"
+      company "The University of Zagreb"
+      start_date "2008"
+      end_date "2012"
+    end
+  end
+
+  group "Certificates" do
+    entry do
+      title "EDX, BerkleyX: CS169.2x: Advanced Software as a Service"
+    end
+    entry do
+      title "EDX, BerkleyX: CS169.1x: Engineering Software as a Service"
     end
   end
 end
